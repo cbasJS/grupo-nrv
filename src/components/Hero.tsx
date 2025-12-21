7d817861fb92a1be3243f4ea6368be8fe045e2f5
Sebastian Martha <sebas.martha@gmail.com>
2025-12-21 17:01:52 -0600

    Se agregó metodo auto play en el componente hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -28,6 +28,7 @@ const Hero: React.FC<Props> = ({ images, caption }) => {
   const [loaded, setLoaded] = useState(false);
   const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>(
     {
+      loop: true,
       initial: 0,
       slideChanged(s) {
         setCurrentSlide(s.track.details.rel);
@@ -39,7 +40,36 @@ const Hero: React.FC<Props> = ({ images, caption }) => {
         setDetails(s.track.details);
       },
     },
-    []
+    [
+      (slider) => {
+        let timeout: ReturnType<typeof setTimeout>;
+        let mouseOver = false;
+        function clearNextTimeout() {
+          clearTimeout(timeout);
+        }
+        function nextTimeout() {
+          clearTimeout(timeout);
+          if (mouseOver) return;
+          timeout = setTimeout(() => {
+            slider.next();
+          }, 2000);
+        }
+        slider.on("created", () => {
+          slider.container.addEventListener("mouseover", () => {
+            mouseOver = true;
+            clearNextTimeout();
+          });
+          slider.container.addEventListener("mouseout", () => {
+            mouseOver = false;
+            nextTimeout();
+          });
+          nextTimeout();
+        });
+        slider.on("dragStarted", clearNextTimeout);
+        slider.on("animationEnded", nextTimeout);
+        slider.on("updated", nextTimeout);
+      },
+    ]
   );
 
   function scaleStyle(idx: number) {
